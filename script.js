90f5d812036569d24369159cf3a0e6cc30066c02
dwilliams64 <dwilliams_64@yahoo.com>
2020-09-24 21:02:39 -0500

    setup api call

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+async function getPhoto () {
+    const pictureCount = 10;
+    const apiKey = "vscw6nORBCCN5yF_URyeRiN1u8WNV3mdltE5NxMThYA";
+    const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${pictureCount}`;
+    
+    try {
+
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        console.log(data);
+
+    } catch (error) {
+        console.log(error);
+    }
+
+    
+
+    
+
+}
+
+getPhoto();
